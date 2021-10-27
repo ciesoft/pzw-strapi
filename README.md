365a8088513e1b39a96daf89bffb2fc38e057bc7
grzegorz <cgrzegorz@gmail.com>
2021-10-27 14:31:53 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# pzw-strapi
+# Strapi application
+
+A quick description of your strapi application
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b5ef5bf0-9836-4371-916a-a05ef5a510f8'
+};
