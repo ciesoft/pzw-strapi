6d48d15940be8147a2e43a8eb526000742723b50
grzegorz <cgrzegorz@gmail.com>
2021-10-27 15:01:56 +0200

    update

--- config/database.js
+++ config/database.js
@@ -18,3 +18,24 @@ module.exports = ({ env }) => ({
     },
   },
 });
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'mongoose',
+//       settings: {
+//         host: env('DATABASE_HOST', 'cluster0.4kbok.mongodb.net'),
+//         srv: env.bool('DATABASE_SRV', true),
+//         port: env.int('DATABASE_PORT', 27017),
+//         database: env('DATABASE_NAME', 'pzw-strapi'),
+//         username: env('DATABASE_USERNAME', 'Admin'),
+//         password: env('DATABASE_PASSWORD', 'Karolek1marca'),
+//       },
+//       options: {
+//         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+//         ssl: env.bool('DATABASE_SSL', true),
+//       },
+//     },
+//   },
+// });
